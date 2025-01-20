ee0400bda12d2c169299b603ce50283665c01f4f
femke77 <megan.meyers.388@gmail.com>
2025-01-19 18:52:51 -0800

    updated sequelize settings

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -12,7 +12,6 @@ const sequelize = process.env.DB_URL
       process.env.DB_USER || '',
       process.env.DB_PASSWORD,
       {
-        host: 'localhost',
         dialect: 'postgres',
         dialectOptions: {
           ssl: {     

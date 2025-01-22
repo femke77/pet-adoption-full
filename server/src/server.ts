55c46979be0efa420ce0b1f0f706b8145a1c1825
femke77 <megan.meyers.388@gmail.com>
2025-01-21 21:34:35 -0800

    added express-winston logging

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,11 +10,16 @@ import { sequelize } from './models/index.js';
 dotenv.config();
 import { fileURLToPath } from 'node:url';
 import path from 'node:path';
+import winston from 'winston'
+import expressWinston from 'express-winston'
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-app.set('trust proxy', 1);
+
+
+// app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3004;
 const SequelizeSessionStore = SequelizeStore(session.Store);
 const sess = {
@@ -59,6 +64,22 @@ app.use(express.urlencoded({ extended: true }));
 //   }),
 // );
 // app.options('*', cors()); //preflight
+
+
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console()
+  ],
+  // format: winston.format.combine(
+  //   winston.format.colorize(),
+  //   winston.format.json()
+  // ),
+  meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+  msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+  expressFormat: false, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+  colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+  // ignoreRoute: function (_req, _res) { return false; } // optional: allows to skip some log messages based on request and/or response
+}));
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {

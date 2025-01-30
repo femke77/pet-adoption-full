c11e30f4984503dc12a9307f9635671d85291f23
femke77 <megan.meyers.388@gmail.com>
2025-01-29 22:43:14 -0800

    success page

--- client/src/pages/Success.tsx
+++ client/src/pages/Success.tsx
@@ -1,7 +1,11 @@
+import image from '../assets/images/blue-bg-group.jpg'
+
 const Success = () => {
   return (
-    <div>
-      <h1>Success</h1>
+    <div className="success">
+           <h2 className='font-cuteCat text-6xl md:text-7xl lg:text-8xl text-center font-bold pt-3  mb-2 px-2'>
+           Thank you for your donation! </h2>
+          
     </div>
   );
 };

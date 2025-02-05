89f121c6fc0ccc33f79599bbac71936e63187ebb
femke77 <megan.meyers.388@gmail.com>
2025-02-04 19:42:04 -0800

    success page better error handling

--- client/src/pages/Success.tsx
+++ client/src/pages/Success.tsx
@@ -5,7 +5,7 @@ const Success = () => {
   const { session_id } = useParams<{ session_id: string }>();
   const { data, isLoading, isError } = useStatusCheck(session_id!);
   const { payment_status, amount_total, customer_details } = data || {};
-  const amount = Math.round(amount_total / 100).toFixed(2);
+  const amount = amount_total ? Math.round(amount_total / 100).toFixed(2) : '0.00';
 
   if (isLoading)
     return (
@@ -13,7 +13,7 @@ const Success = () => {
     );
   if (isError)
     return (
-      <div className='text-red ml-12'>
+      <div className='text-red-500 ml-12'>
         An error has occurred. Try again later.
       </div>
     );
@@ -24,7 +24,7 @@ const Success = () => {
         <div className='success'>
           <h2 className='font-cuteCat mt-4 text-3xl/[1.5] md:text-5xl/[1.5] lg:text-6xl/[1.5] text-center font-bold pt-3 mx-12  mb-2 px-2'>
             Your donation of ${amount} has been received. <br />
-            Thank you for your donation, {customer_details.name}! 🐾
+            Thank you for your donation, {customer_details?.name}! 🐾
           </h2>
         </div>
       ) : (

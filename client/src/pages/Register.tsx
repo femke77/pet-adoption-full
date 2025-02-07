f1b1ba78e6dac3caf584aa2f129cbf36fd33ef81
femke77 <megan.meyers.388@gmail.com>
2025-02-06 20:01:48 -0800

    stopping email autofill on username input on register form

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -110,13 +110,15 @@ const Register: React.FC = () => {
                 Username
               </label>
               <div className='mt-2'>
+              
+              <input type="text" name="fake-user" autoComplete="username" style={{ display: "none" }} />   {/* dummy input to stop chrome autofilling email into username */}
                 <input
                   className='block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6'
                   type='text'
                   id="username"
                   placeholder='luckydog123'
                   name='username'
-                  autoComplete='username'
+                  autoComplete='off'
                   required
                   onChange={(e) => {
                     setFormState({ ...formState, username: e.target.value });
